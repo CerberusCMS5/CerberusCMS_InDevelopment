df407e2866a20dc2ae69e963de8eed509c9a8fdf
Cerberus Content Management System <CerberusCMS5@Proton.me>
2024-10-29 15:24:37 -0700

    Update Architect.php

--- Projects/Software Projects/CerberusCMS/PHP 8/PHP 8.3/MySQL - Improved/Version Number — 5/Build Number — 01/Edit Number — 0001/Manual Edit Number — 001/Cerberus/Architect.php
+++ Projects/Software Projects/CerberusCMS/PHP 8/PHP 8.3/MySQL - Improved/Version Number — 5/Build Number — 01/Edit Number — 0001/Manual Edit Number — 001/Cerberus/Architect.php
@@ -2501,6 +2501,8 @@ echo ("
 	<INPUT TYPE=\"password\" NAME=\"Architect_Administration_Account_Password_2\" MAXLENGTH=\"50\"><BR>
 			* <I>Administration Account :: Birth Date</I>:<BR>
 	<INPUT TYPE=\"date\" NAME=\"Architect_Administration_Account_BirthDate\" MAXLENGTH=\"50\"><BR>
+			* <I>Administration Account :: Recovery Key</I>
+	<INPUT TYPE=\"text\" VALUE=\"$_ARCHITECT_POST_ADMINISTRATOR_ACCOUNT_RECOVERY_KEY\" MAXLENGTH=\"50\"><BR>
 	<INPUT TYPE=\"submit\" VALUE=\"Install Administrator\">
 	</FORM><BR>
 ");
